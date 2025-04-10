27d307f53af1e1b92965edd98317163132592b30
Raspberry-Jam <raspberryjam.chivers@gmail.com>
2025-04-10 18:31:27 +1000

    Fill out Contact entity fields

--- src/main/java/we/code/demo/model/entity/Contact.java
+++ src/main/java/we/code/demo/model/entity/Contact.java
@@ -1,4 +1,19 @@
 package we.code.demo.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
 public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Contact(String firstName, String lastName, String phoneNumber, String emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
 }
