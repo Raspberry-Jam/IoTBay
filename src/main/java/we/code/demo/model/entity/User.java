f2c9cfe6ac0a7294b2675e87e149d7e9a0c070c7
Raspberry-Jam <raspberryjam.chivers@gmail.com>
2025-04-10 22:49:24 +1000

    Store session data

--- src/main/java/we/code/demo/model/entity/User.java
+++ src/main/java/we/code/demo/model/entity/User.java
@@ -3,12 +3,16 @@ package we.code.demo.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class User {
     private final String username;
     @Setter private String password;
-    @Setter Contact contact;
-    @Setter Address address;
+    @Setter private Contact contact;
+    @Setter private Address address;
+    @Setter private String sessionToken;
+    @Setter private LocalDateTime sessionExpiry;
 
     public User(String username, String password, Contact contact, Address address) {
         this.username = username;
