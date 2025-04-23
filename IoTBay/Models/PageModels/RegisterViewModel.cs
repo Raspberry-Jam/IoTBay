9c593965aee8d2bb3d5d1730709fc9d55ad92536
James <raspberryjam.chivers@gmail.com>
2025-04-24 08:06:52 +1000

    Create new Register view

---
+++ IoTBay/Models/PageModels/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+namespace IoTBay.Models.PageModels;
+
+public class RegisterViewModel
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+    public string? GivenName { get; set; }
+    public string? FamilyName { get; set; }
+}
