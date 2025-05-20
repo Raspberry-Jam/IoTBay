30ee1d69ffb99ad62623484f5960c1f428370a95
James <raspberryjam.chivers@gmail.com>
2025-05-21 04:03:01 +1000

    Add a query for getting all products with a positive ID (no fallback rows)

--- IoTBay/Repositories/ProductRepository.cs
+++ IoTBay/Repositories/ProductRepository.cs
@@ -1,6 +1,16 @@
 using IoTBay.Models;
 using IoTBay.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace IoTBay.Repositories;
 
-public class ProductRepository(AppDbContext db) : BaseRepository<Product>(db);
+public class ProductRepository(AppDbContext db) : BaseRepository<Product>(db)
+{
+    public async Task<List<Product>> GetAllNoFallback()
+    {
+        var query = from p in db.Products
+            where p.ProductId >= 1
+            select p;
+        return await query.ToListAsync();
+    }
+}
