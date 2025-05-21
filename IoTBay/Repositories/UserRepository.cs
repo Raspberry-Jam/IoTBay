6a0b229b7963c6f56b30bf92f1ec298e6146b2a6
James <raspberryjam.chivers@gmail.com>
2025-05-21 19:28:40 +1000

    Add LoadFullUser methods to eager load all navigation properties of a User entity

--- IoTBay/Repositories/UserRepository.cs
+++ IoTBay/Repositories/UserRepository.cs
@@ -23,6 +23,24 @@ public class UserRepository(AppDbContext db) : BaseRepository<User>(db)
         return await query.ToListAsync();
     }
 
+    public async Task<User?> LoadFullUser(User user) => await LoadFullUser(user.UserId);
+
+    public async Task<User?> LoadFullUser(int userId)
+    {
+        var query = from u in _db.Users
+            where u.UserId == userId
+            select u;
+
+        return await query
+            .Include(u => u.Contact)
+            .Include(u => u.Orders)
+            .Include(u => u.PaymentMethods)
+            .Include(u => u.ShipmentMethods)
+            .Include(u => u.UserAccessEvents)
+            .Include(u => u.UserCartProducts)
+            .FirstOrDefaultAsync();
+    }
+
     public async Task<IEnumerable<UserAccessEvent>> GetUserAccessEvents(User user)
     {
         var query = from e in _db.UserAccessEvents
