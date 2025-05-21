0620b7f52769d9dd27763b19d4c5ffcc649337e7
James <raspberryjam.chivers@gmail.com>
2025-05-21 23:03:19 +1000

    Remove session set in registration

--- IoTBay/Controllers/UserController.cs
+++ IoTBay/Controllers/UserController.cs
@@ -85,7 +85,6 @@ public class UserController(ILogger<UserController> logger, UserRepository userR
             GivenName = user.Contact!.GivenName,
             Role = user.Role
         };
-        SessionUtils.SetObjectAsJson(HttpContext.Session, "currentUser", userSessionDto);
         
         // Send them to the Welcome View
         return View("Welcome", user);
@@ -140,9 +139,19 @@ public class UserController(ILogger<UserController> logger, UserRepository userR
         
         // Set the session data
         SessionUtils.SetObjectAsJson(HttpContext.Session, "currentUser", userSessionDto);
+        
+        user.UserAccessEvents.Add(new UserAccessEvent
+        {
+            EventTime = DateTime.Now,
+            EventType = AccessEventType.Login,
+            UserId = user.UserId
+        });
+        
+        userRepository.Update(user);
+        await userRepository.SaveChangesAsync();
 
-        // Send the user to the welcome page
-        return View("Welcome", user);
+        // Send the user to the home page
+        return RedirectToAction("Index", "Home");
     }
     
     // Allow both HttpGet and HttpPost
@@ -151,9 +160,153 @@ public class UserController(ILogger<UserController> logger, UserRepository userR
     /// </summary>
     /// <returns>Home Index View</returns>
     [AuthenticationFilter]
-    public IActionResult Logout()
+    public async Task<IActionResult> Logout()
     {
+        var sessionDto = SessionUtils.GetObjectFromJson<UserSessionDto>(HttpContext.Session, "currentUser");
+        // We aren't going to handle null errors here because the user is logging out. If there is a problem, then
+        // it's going to go away when the session is cleared.
+        if (sessionDto != null)
+        {
+            var user = await userRepository.LoadFullUser(sessionDto.UserId);
+            if (user != null)
+            {
+                user.UserAccessEvents.Add(new UserAccessEvent // Add a logout access event to the user
+                {
+                    EventTime = DateTime.Now,
+                    EventType = AccessEventType.Logout,
+                    UserId = user.UserId
+                });
+                await userRepository.SaveChangesAsync();
+            }
+        }
         HttpContext.Session.Clear();
         return RedirectToAction("Index", "Home");
+    }
+
+    /// <summary>
+    /// Check browser session state to authenticate access, create a UserSettingsViewModel from the user session
+    /// data, and send it to the Settings View to render.
+    /// </summary>
+    /// <returns>Settings View</returns>
+    [HttpGet]
+    [AuthenticationFilter]
+    public async Task<IActionResult> Settings()
+    {
+        // Get the session data from the request
+        var sessionDto = SessionUtils.GetObjectFromJson<UserSessionDto>(HttpContext.Session, "currentUser");
+        if (sessionDto == null) // If they managed to execute this action without session data, something went wrong
+        {
+            logger.LogError("Settings page authenticated with an invalid session");
+            return RedirectToAction("Error", "Home");
+        }
+        
+        // Get the user corresponding to the session data
+        var user = await userRepository.LoadFullUser(sessionDto.UserId);
+        if (user == null) // If they have session data without a corresponding user, something went wrong
+        {
+            logger.LogError("Settings page authenticated with a non-existent user");
+            return RedirectToAction("Error", "Home");
+        }
+
+        var accessEvents = user.UserAccessEvents
+            .Select(e => new UserAccessEventViewModel
+            {
+                EventTime = e.EventTime,
+                EventType = e.EventType
+            });
+        
+        // Create a settings view model and populate it with the important data
+        var model = new UserSettingsViewModel
+        {
+            Email = user.Contact!.Email,
+            GivenName = user.Contact!.GivenName,
+            Surname = user.Contact!.Surname,
+            PhoneNumber = user.Contact!.PhoneNumber,
+            AccessEvents = accessEvents,
+            UserId = user.UserId
+        };
+        
+        // Send the model to the view for rendering
+        return View(model);
+    }
+
+    /// <summary>
+    /// Validate form input and update user's details if everything is valid.
+    /// </summary>
+    /// <param name="model">Model from Settings View</param>
+    /// <returns>Settings View with success or error messages attached</returns>
+    [HttpPost]
+    [AuthenticationFilter]
+    public async Task<IActionResult> Settings(UserSettingsViewModel model)
+    {
+        // Check error state of the model, and redirect to index if the model is malformed
+        if (!ModelState.IsValid)
+        {
+            foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+            {
+                logger.LogError(error.ErrorMessage);
+            }
+            logger.LogError("Register was passed with an invalid model");
+            return View(model);
+        }
+        
+        var user = await userRepository.LoadFullUser(model.UserId);
+
+        if (user == null)
+        {
+            logger.LogError("Authenticated a user settings update without a valid user ID");
+            return RedirectToAction("Error", "Home");
+        }
+        
+        if (model.NewPassword != null) // Change their password
+        {
+            if (model.OldPassword == null) // They need to entire their old password to change their password
+            {
+                ModelState.AddModelError("needOldPassword", "Please enter your current password to change it.");
+                return View(model);
+            }
+            // Check if their old password matches
+            if (HashUtils.VerifyPassword(user.PasswordHash!, model.OldPassword, user.PasswordSalt!))
+            {
+                // If it does, hash the new password and update the user entity with it
+                var passwordHash = HashUtils.HashPassword(model.NewPassword, out var salt);
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = salt;
+            }
+        }
+
+        if (model.Email != null) // Change their email
+        {
+            // Check if the email is already in use
+            var emailMatch = await userRepository.GetByEmail(model.Email);
+            if (emailMatch != null && emailMatch.UserId != user.UserId) // This email belongs to a different user
+            {
+                // If it is, send them back with an error
+                ModelState.AddModelError("emailInUse", "This email is unavailable, please choose a different one.");
+                return View(model);
+            }
+
+            // Update the user entity with the new email
+            user.Contact!.Email = model.Email;
+        }
+
+        // Update their phone number (allow null values, if they want to remove their phone number from their account).
+        user.Contact!.PhoneNumber = model.PhoneNumber;
+
+        // Update their given name if it is not null. Do not allow empty names.
+        if (model.GivenName != null)
+        {
+            user.Contact!.GivenName = model.GivenName;
+        }
+        
+        // Update their surname.
+        user.Contact!.Surname = model.Surname;
+
+        // Track and update the user entity in the current repository context
+        userRepository.Update(user);
+        await userRepository.SaveChangesAsync();
+
+        ViewData["SuccessMessage"] = "Details updated successfully!";
+        return View(model);
     }
 }
